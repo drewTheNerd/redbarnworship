06e8380d554dd3ab74cad1fc55c6b338ef4b5e01
DrewShipps <trafficminister@gmail.com>
2025-07-19 22:17:24 -0500

    initial deploy

---
+++ resources/js/pages/ItsARedBarn.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+
+    <div class="h-screen flex items-center justify-center">
+        <h1 class="text-4xl">
+            It's a Red Barn.
+        </h1>
+    </div>
+</template>
