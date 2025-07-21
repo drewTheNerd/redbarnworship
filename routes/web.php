1755da53329ffe0a038306f1f26be924f54a67bc
DrewShipps <trafficminister@gmail.com>
2025-07-20 22:29:55 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,9 @@ Route::get('/album', function () {
     return Inertia::render('AlbumLaunchHomepage');
 })->name('album');
 
+
+
+// fallback route for any other page. if a user attempts to load anything else besides the routes defined above, we'll fallback to redirecting to the homepage. this MUST be the last route in this file
+Route::fallback(function () {
+    return redirect('/');
+});
